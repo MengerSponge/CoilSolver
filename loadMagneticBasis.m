caf6eb9e28185d8f6d24ad371c6ae520f78380c7
Austin R <aureid@gmail.com>
2020-07-02 20:55:44 -0400

    Update loadMagneticBasis.m

--- loadMagneticBasis.m
+++ loadMagneticBasis.m
@@ -1,4 +1,23 @@
-fid=fopen('MagneticBasis.txt');
+function loadMagneticBasis(model,Vm, maxl, basisfunctions, bubble)
+
+if ~exist('Vm','var') || isempty(Vm)
+  Vm='Vm';
+end
+
+if ~exist('maxl','var') || isempty(maxl)
+  maxl=10;
+end
+
+if ~exist('basisfunctions','var') || isempty(basisfunctions)
+  basisfunctions = 'MagneticBasis.txt';
+end
+
+if ~exist('bubble','var') || isempty(bubble)
+  bubble = '/(0.5[m])^';
+end
+
+
+fid=fopen(basisfunctions);
 tline = fgetl(fid);
 basisl = cell(0,1);
 while ischar(tline)
@@ -8,9 +27,40 @@ end
 fclose(fid);
 
 basis = cell(0,1);
-
-maxl = length(basisl);
-
+emm = cell(0,1);
 for l = 1:maxl
-    basis{l,1} = split(basisl{l}(2:end-1),', ');
+    % wrap each harmonic in parens and multiply it by scalar potential
+    % divide by r^l
+    basis{l,1} = strcat(strcat('(',split(basisl{l}(2:end-1),', ')),[')*' Vm bubble num2str(l)]);
+    emstring = cell(1,2*l+1);
+    for m = 1:2*l+1
+        emstring{m} = ['l=' num2str(l) ' m=' num2str(m-l-1)];
+    end
+    emm{l,1} = emstring';
 end
+
+% Flatten basis*Vm cell array and l,m label cell array
+exprcell = vertcat(basis{:});
+emmcell = vertcat(emm{:});
+
+% model.result.numerical.create('int1', 'IntSurface');
+% model.result.numerical('int1').selection.named('geom1_csel1_bnd');
+% model.result.numerical('int1').set('probetag', 'none');
+% 
+% model.result.numerical('int1').set('table', 'tbl1');
+% model.result.numerical('int1').set('expr', {'Vm*x' 'Vm*y' 'Vm*z' 'Vm*(x*y)'});
+% model.result.numerical('int1').set('unit', {'m^3*A' 'm^3*A' 'm^3*A' 'm^4*A'});
+% model.result.numerical('int1').set('descr', {'' '' '' ''});
+% model.result.numerical('int1').setResult;
+
+% model.result.numerical.create('int1', 'IntSurface');
+% model.result.numerical('int1').selection.named('geom1_csel1_bnd');
+% model.result.numerical('int1').set('probetag', 'none');
+
+% model.result.numerical('int1').set('table', 'tbl1');
+model.result.numerical('int1').set('expr',exprcell');
+model.result.numerical('int1').set('descr', emmcell');
+model.result.numerical('int1').setResult;
+
+end
+% end
